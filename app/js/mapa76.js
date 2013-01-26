a0f5d3e6c90e0f6fc9efafa24fbcd6f20d4ac06d
opensas <opensas@gmail.com>
2013-01-26 15:04:26 -0300

    test page

---
+++ app/js/mapa76.js
@@ -0,0 +1,12 @@
+(function () {
+
+  // WebActivities
+  var mapa76Test1 = document.querySelector("#mapa76-test1");
+    if (mapa76Test1) {
+    mapa76Test1.onclick = function () {
+      document.location = "../Examples/find_nearest_locations.html";
+    };
+  };
+
+
+})();
